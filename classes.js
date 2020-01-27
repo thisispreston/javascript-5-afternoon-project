bc6b82608a71720e2d5af13f1c19666ac4e04e32
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-01-27 15:04:48 -0700

    classes are done

--- classes.js
+++ classes.js
@@ -29,8 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
+class Employee {
+  constructor (first_name, last_name, email, age) {
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+    makeWidget () {
+      return `${this.first_name} ${this.last_name} Widget`
+    }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -47,8 +56,18 @@
   Call your new class Manager
 */
 
-//Code Here
-
+class Manager extends Employee {
+  constructor (first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+  hire = (employee) => { 
+    return this.reports.push(employee)
+  }
+  fire = (i) => {
+    return this.reports.splice(i, 1)
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -71,9 +90,39 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager {
+  constructor (first_name, last_name, email, age) {
+    super(first_name, last_name, email, age)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+  changeTitle = () => {
+    if (this.reports.length === 0) {
+      this.title
+    } else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager'
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = 'Mostly Manager'
+    } else if ( this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = 'Manager'
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus'
+    } else if (this.reports.length >= 101) {
+      this.title = 'Bestest Manager'
+    }
+  }
 
+  hire = (employee) => {
+    this.reports.push(employee)
+    this.changeTitle()
+  }
 
+  fire = (i) => {
+    this.reports.splice(i, 1)
+    this.changeTitle()
+    this.bonus += 100
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
